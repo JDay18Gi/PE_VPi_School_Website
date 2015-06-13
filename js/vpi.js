d22ab8bc4112d33392a8c5df2d14d4a058b9a560
Jason D. G <JDay18Gi@users.noreply.github.com>
2015-06-13 14:29:03 -0400

    add dropdown menu

---
+++ js/vpi.js
@@ -0,0 +1,18 @@
+
+$(document).ready(function(){
+
+	$("#menu-solution").on("mouseenter", function(){
+		$("#solution-dd").show();
+	});
+	$("#menu-solution").on("mouseleave", function(){
+		$("#solution-dd").hide();
+	});
+
+	$("#solution-dd").on("mouseenter", function(){
+		$("#solution-dd").show();
+	});
+	$("#solution-dd").on("mouseleave", function(){
+		$("#solution-dd").hide();
+	});
+
+});
